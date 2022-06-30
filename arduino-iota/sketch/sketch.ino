8c0281932d44d2cf9fbca8b4d7ddab7c404d4948
javi <javi17pm@gmail.com>
2022-06-30 19:57:27 +0200

    testing sketch with static data

---
+++ arduino-iota/sketch/sketch.ino
@@ -0,0 +1,45 @@
+// set interval for sending messages (milliseconds)
+const long interval = 5000;
+unsigned long previousMillis = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // avoids blocking if none monitor serial is found
+  delay(1500);
+
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+
+    // save the last time a message was sent
+    previousMillis = currentMillis;
+
+    // Mostrar los datos obtenidos por el monitor serie
+    Serial.print("temperatura: ");
+    Serial.print("30");
+    Serial.print(", ");
+    Serial.print("humedad: ");
+    Serial.print("27");
+    Serial.println();
+
+    delay(20000);
+    
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  }
+}
